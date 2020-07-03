f2a64ba6eee1b70faeb8cd6054200953e9a51cb5
ISol <599304776@qq.com>
2020-07-03 14:56:38 +0800

    Tags 组件 的选中和取消

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad/>
     <Types />
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </layout>
 </template>
 
-<script lang="ts">
+<script>
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
@@ -16,6 +16,11 @@
   export default {
     name: 'money',
     components: {Tags, Notes, Types, NumberPad},
+    data(){
+      return {
+        tags: ['衣','食','住','行']
+      }
+    }
   };
 </script>
 
