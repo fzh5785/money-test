d6aeadeff5ffd880536625f159fcaf27410745af
ISol <599304776@qq.com>
2020-07-07 14:49:32 +0800

    消除 Tags 的警告，但还存在 bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,9 +4,9 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag"
+      <li v-for="tag in dataSource" :key="tag.id"
           @click="toggle(tag)" :class="{selected: selectedTags.indexOf(tag)>=0}"
-      >{{tag}}</li>
+      >{{tag.name}}</li>
     </ul>
   </div>
 </template>
