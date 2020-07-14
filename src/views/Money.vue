69e4052bef2160ef7e9cef162a70eebbdfb0b073
ISol <599304776@qq.com>
2020-07-14 21:07:49 +0800

    完成列表的 css

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <layout class-prefix="layout">
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
     </div>
@@ -12,19 +13,21 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components: {Tags, FormItem, Types, NumberPad},
+    components: {Tabs, Tags, FormItem,  NumberPad},
   })
   export default class Money extends Vue {
     get recordList() {
       return this.$store.state.recordList;
     }
 
+    recordTypeList = recordTypeList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -51,7 +54,7 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,15 +1,105 @@
 <template>
   <layout>
-    statistics.vue
+    <Tabs class-prefix="type" :data-source="recordTypeList"
+          :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList"
+          :value.sync="interval"/>
+    <div>
+      <ol>
+        <li v-for="(group,index) in result" :key="index">
+          <h3 class="title">{{group.title}}</h3>
+          <ol>
+            <li v-for="item in group.items" :key="item.id"
+            class="record">
+              <span>{{tagString(item.tags)}}</span>
+              <span class="notes">{{item.notes}}</span>
+              <span>￥{{item.amount}} </span>
+            </li>
+          </ol>
+        </li>
+      </ol>
+    </div>
   </layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics',
-  };
+  import Vue from 'vue';
+  import Tabs from '@/components/Tabs.vue';
+  import {Component} from 'vue-property-decorator';
+  import intervalList from '@/constants/intervalList';
+  import recordTypeList from '@/constants/recordTypeList';
+
+  @Component({
+    components: {Tabs},
+  })
+  export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join('');
+    }
+
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = { title: string; items: RecordItem[] }
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || {title: date, items: []};
+        hashTable[date].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    beforeCreated() {
+      this.$store.commit('fetchRecords');
+    }
+
+    type = '-';
+    interval = 'day';
+    intervalList = intervalList;
+    recordTypeList = recordTypeList;
+  }
 </script>
 
 <style lang="scss" scoped>
+  %item{
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+  .title{
+    @extend %item
+  }
+  .record {
+    background: white;
+    @extend %item
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+  ::v-deep {
+    .type-tabs-item {
+      background: white;
 
+      &.selected {
+        background: #c4c4c4;
+
+        &::after {
+          display: none;
+        }
+      }
+    }
+
+    .interval-tabs-item {
+      height: 36px;
+      font-size: 18px;
+    }
+  }
 </style>
